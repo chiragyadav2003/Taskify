a071561e1b17e7a86f8fe2d5aeb5b1c12cac02b0
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-15 07:01:56 +0530

    custom font adding using tailwind's "cn"

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,28 @@
-import { Button } from "@/components/ui/button";
-import { Medal } from "lucide-react"
 import Link from "next/link";
+import { Medal } from "lucide-react"
+import LocalFont from "next/font/local"
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+const headingFont = LocalFont({
+    src: "../../public/fonts/font.woff2"
+})
+
+const textFont = Poppins({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800"]
+})
+
 
 const MarketingPage = () => {
     return (
         <div className="flex items-center justify-center flex-col">
-            <div className="flex items-center justify-center flex-col">
+            <div className={cn(
+                "flex items-center justify-center flex-col"
+                , headingFont.className
+            )}>
                 <div className="flex mb-4 p-4 shadow-lg bg-amber-100 text-amber-700 rounded-full uppercase items-center justify-center">
                     <Medal className="size-6 mr-2" />
                     No 1 task management
@@ -17,7 +34,10 @@ const MarketingPage = () => {
                     work forward.
                 </div>
             </div>
-            <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+            <div className={cn(
+                "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+                "textFont.className"
+            )}>
                 Collaborate, manage projects, and reach new productivity peaks. From high rise to the home office, the way your team works is unique - accomplish it all with Taskify.
             </div>
             <Button className="mt-6" size={"lg"} asChild>
