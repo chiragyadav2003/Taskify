2b0c085ab4364ea8736bfb5c0886cd9c1330e6cf
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-15 15:23:56 +0530

    layout for signin and signup

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,9 @@
+const CLerkLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="flex justify-center items-center h-full w-full">
+            {children}
+        </div>
+    )
+}
+
+export default CLerkLayout;
