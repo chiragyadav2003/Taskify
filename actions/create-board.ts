bc4017a846e5c68e56ebd2f04dca5c4644d1482a
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-16 23:45:37 +0530

    adding zod validation in create-board server action and throwing custom error from backend

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -4,18 +4,49 @@ import { z } from "zod"
 
 import prisma from "@/db/db";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export type State = {
+    errors?: {
+        title?: string[]
+    };
+    message?: null | string;
+}
 
 const createBoardSchema = z.object({
-    title: z.string()
+    title: z.string().min(3, {
+        message: "Minimum length of 3 letters is required !"
+    })
 })
 
-export async function create(formData: FormData) {
+export async function create(prevState: State, formData: FormData) {
 
-    const res = createBoardSchema.parse({ title: formData.get("title") })
-    await prisma.board.create({
-        data: {
-            title: res.title
+    const validatedFields = createBoardSchema.safeParse({ title: formData.get("title") })
+
+    if (!validatedFields.success) {
+        //NOTE - this syntax shows partivcular field error
+        // { title: [ 'Minimum length of 3 letters is required !' ] }
+
+        // console.log(validatedFields.error.flatten().fieldErrors)
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: "Missing fields"
         }
-    })
-    revalidatePath("/organization/org_2f8gMuiCkzvdqEkbmUi7UvJmErX")
+    }
+
+    const { title } = validatedFields.data
+    try {
+        await prisma.board.create({
+            data: {
+                title: title
+            }
+        })
+    } catch (error) {
+        return {
+            message: "Database Error"
+        }
+    }
+    revalidatePath("/organization/org_2f8gMuiCkzvdqEkbmUi7UvJmErX");
+    redirect("/organization/org_2f8gMuiCkzvdqEkbmUi7UvJmErX")
+
 }
